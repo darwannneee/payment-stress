839c068249ba4af63081cdf5f3e1bfe59a6dea2d
darwannneee <darwanchannel1@gmail.com>
2023-12-08 15:17:29 +0700

    correction about TIP

--- src/Components/Home/Payment.js
+++ src/Components/Home/Payment.js
@@ -4,7 +4,7 @@ import DownUpImage from "../../assets/img/down-up.png"
 
 function Payment() {
     let [Month, setMonth] = useState(0);
-    let [Tip, setTip] = useState(0.1);
+    let [Tip, setTip] = useState(0);
     const [discordId, setDiscordId] = useState(''); // State to store Discord ID\
     const eselonList = ['1', '2', '3', '4' /* Add other Eselon Discord IDs here */];
 
@@ -24,8 +24,22 @@ function Payment() {
     };
 
     // Declaration Variabel
-    let totalSubcription = Month * 5;
-    let totalPayment =  totalSubcription + Tip;
+    let totalSubcription = 0;
+    let totalPayment = 0;
+
+    // Logic Total Subscription and Payment
+    if(Month === undefined) {
+        totalSubcription = 0
+        Month = 0;
+    } else if(Month > 0 && Tip === 0) {
+        totalSubcription = Month * 5;
+        totalPayment = totalSubcription;
+    } else if(Tip > 0 && Month === 0) {
+        totalPayment = Tip
+    }else if(Month > 0 && Tip > 0) {
+        totalSubcription = Month * 5;
+        totalPayment = totalSubcription + Tip;
+    }
 
     // Function Change if click button
     const handleInputMonth = (event) => {
@@ -39,6 +53,15 @@ function Payment() {
     const handleDecreaseMonth = () => {
         setMonth((Month) => Math.max(Month - 1, 1)); // Ensure month doesn't go below 1
     };
+
+    const handleIncreaseTIP = () => {
+        setTip((Tip) => Tip + 1.0);
+    };
+    
+    const handleDecreaseTIP = () => {
+        setTip((Tip) => Math.max(Tip - 0.1, 0.2)); // Ensure month doesn't go below 1
+    };
+
 
     return(
         <div className="md:border-l-2 border-t-2 md:border-t-0 border-white mt-16 md:mt-0 md:pl-28">
@@ -72,21 +95,11 @@ function Payment() {
                 </div>
             </div>
 
-            {/* Form TIP */}
-            <div className="pt-8 md:pt-4 md:flex">
-                <div className="fontDescription">
-                    <h1>TIP</h1>
-                    <input className="w-64 h-9 md:w-24 md:h-10 rounded-xl bg-slate-200 md:mt-2 text-black text-center"
-                    value={Tip}
-                    onChange={(e) => setTip(e.target.value)}></input>
-                    <h1 className="text-violet-500 text-xs mt-1">*please enter your discord id correctly.</h1>
-                </div>
-            </div>
             {/* Line */}
             <hr className="w-full border-white border mt-14 md:mt-10"></hr>
 
             {/* Display message to enter Discord ID if it's not set */}
-            {!discordId && <h1 className="text-center fontDescription mt-5 md:mt-10 text-violet-500">Input your Discord ID first.</h1>}
+            {!discordId && <h1 className="text-center fontDescription md:mt-10 text-violet-500">Input your Discord ID first.</h1>}
             
             {isEselon ? (
                 <h1 className="text-center fontDescription md:mt-10 text-violet-500">Account Discord Already Eselon.</h1>
@@ -100,14 +113,23 @@ function Payment() {
                     <div className="grid grid-cols-2 md:pt-5">
                         <h1>Tip</h1>
                         <div className="flex">
-                            <h1 className="pl-24">{Tip}$</h1>    
+                            <h1 className="pl-24">
+                            <input className="w-64 h-9 md:w-10 md:h-6 rounded-xl bg-slate-200 md:mt-2 text-black text-center"
+                            value={Tip}
+                            onChange={(e) => setTip(e.target.value)}></input></h1>
+                            <div className="md:pl-2 flex pl-1">
+                                <button onClick={handleIncreaseTIP}>
+                                    <img src={ArrowUpImage} className="w-5 md:w-3" alt="Arrow Up"/>
+                                </button>
+                                <button onClick={handleDecreaseTIP}>
+                                    <img src={DownUpImage} className="w-5 md:w-3" alt="Arrow Down"/>
+                                </button>
+                            </div>
                         </div>
                     </div>
                     <button className="bg-violet-500 w-full h-8 md:h-10 rounded-xl mt-4 md:mt-14 fontDescription">Pay {totalPayment}$</button>
                 </div>
-            )}
-            
-            
+            )}  
         </div>
     )
 }
